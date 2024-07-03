d6464fcc65594c3874320127767bdd9eed7bcce5
prasanth-vaindam <107233850+prasanth-vaindam@users.noreply.github.com>
2024-07-03 19:42:49 +0530

    Pranavi's Day 7 Class update and 7pm Batch Pattern Programs

--- Classes/Class Pranavi/Day007_02-July-2024/01 Float DataType.py
+++ Classes/Class Pranavi/Day007_02-July-2024/01 Float DataType.py
@@ -4,8 +4,9 @@ print(f)  # 1.234
 f = 1.2e3  # 1.2 * 1000 = 1200.0
 print(f)
 
-# f = 0B11.01  # this is invalid
+# f = 0B11.01  # SyntaxError: invalid syntax
 # similarly we can't have floating point hexadecimal and octal numbers
 
+# f =  f=0o123.456   # SyntaxError: invalid syntax
 i = 0O12 #
 print(i)
