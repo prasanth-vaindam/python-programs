acb794ddd2a2a0f263cdd0bfd50c3043616a0364
prasanth-vaindam <107233850+prasanth-vaindam@users.noreply.github.com>
2024-07-04 20:07:03 +0530

    Pranavi's Day 7 Class update and 7pm Batch Pattern Programs

---
+++ Classes/Nikitha/Day09_03-June-2024/01.py
@@ -0,0 +1,28 @@
+# program to demonstrate operators
+
+a = 10
+b = 30
+
+print(a+b)  # 40
+print(a-b)  # -20
+
+print(b-a)  # 20
+print(a*b)  # 300
+
+a = 10
+b = 3
+print(a/b)   # 3.333
+print(a//b)  # 3
+
+print(a%b)   # 1
+
+
+a = 100
+a = a + 10
+print(a)  # 110
+
+a += 40  # a = a + 40
+print(a)  # 150
+
+a -= 40
+print(a)  #  110
