5cf99e91bc2fd749207042ad98254e604f617cee
prasanth-vaindam <107233850+prasanth-vaindam@users.noreply.github.com>
2024-07-08 20:39:04 +0530

    Nikitha's day 10 code

---
+++ Classes/Nikitha/Day10_08-July-2024/01 range.py
@@ -0,0 +1,10 @@
+print(range(6))
+print(range(1, 6))
+for i in range(1,11):
+    print(i, "Jai Hind!")
+
+for i in range(1, 11, 3):
+    print(i, "Jai Hind!")
+print("----")
+for i in range(0, 6, 2):
+    print(i, "Jai Hind!")
