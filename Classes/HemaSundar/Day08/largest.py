018c6b0ec323e6c9dfebcaf595b8f95c2d68d535
prasanth-vaindam <vaindam.prasanth@gmail.com>
2026-06-26 09:30:42 +0530

    day  8 class -  list operation

---
+++ Classes/HemaSundar/Day08/largest.py
@@ -0,0 +1,20 @@
+list =  [10,2,30,4,5,6]
+
+list.reverse()
+print(list)
+
+print(max(list))
+print(min(list))
+print(sum(list))
+list.sort()
+print(list)
+print(list[-2])
+list.sort(reverse=True)
+# second largest element
+print(list[1])
+num=[1,2,3,4,3,2,2,1,3,4,5,7,8]
+cnt=num.count(2)
+print("Count of 2 is:",cnt)
+cnt=num.count(10)
+print("Count of 10 is:",cnt)
+
